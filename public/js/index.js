9a7ae8d161ea9d4d207e2615875ad26d4e2199fb
Luisa <luisa.h.morales@gmail.com>
2022-07-20 15:21:58 -0300

    micro desafio 1 terminado

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,9 @@ window.onload = function(){
     let destacado = document.querySelectorAll('p');
     let fondo = document.querySelector('body');
     let enlace = document.querySelector('a');
-    let logo = document.querySelector('.logoDH');
-    let menu = document.querySelector('#menu');
-    let ul= document.querySelector('ul')
+    let logo = document.querySelector('.logoDH'); // capturo el elemento q quiero manipular
+    let menu = document.querySelector('#menu'); // capturo el div con id menu
+   
     
     let nombre = prompt('Ingrese su nombre');
     console.log(nombre);
@@ -35,9 +35,22 @@ window.onload = function(){
 
     container.style.display = 'block';
 
-   logo.addEventListener('click',() => {
+
+   /* 1- En index.ejs, agregar un evento para que cada vez que el usuario haga clic sobre el
+    logo de Digital House se muestre el menú lateral con id="menu". El estilo y el menú
+    lateral ya existe en el proyecto de base. Tips: podemos usar el atributo classList
+    con el método toggle para agregar o quitar la clase class="menu". */
+   logo.addEventListener('click',() => { // agrego un elemento de escucha. onclick
     menu.classList.toggle('mostrar')
    })
+  /* 2-  En index.ejs, agregar un evento que permita ocultar el menú lateral cuando el
+mouse deje el área del menú. */
 
-   
+ menu.addEventListener('mouseout', () => {
+    menu.classList.remove('mostrar')
+
+ })
+ menu.addEventListener('mouseover',() => {
+    menu.classList.add('mostrar')
+ })
 }
--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
@@ -6,7 +6,57 @@ window.onload = function(){
     titulo.classList.add('titulo');
     article.classList.add('fondoTransparente');
     formulario.classList.add('fondoCRUD');
+ 
+  /*  En moviesAdd.ejs, establecer que, cada vez que se pase el mouse por el título
+    'AGREGAR PELÍCULA', este cambie su color. */
+
+    titulo.addEventListener('mouseover', () => {
+       titulo.classList.toggle('fondoTurquesa')
+
+    }) 
+
+    
 
 
 
-}
+  /*  manejadorDeEstado(); */
+};
+
+
+
+/* let estadoSecreto = 0;  // iniciador
+
+let secreto = 'secreto';
+
+let manejadorDeEstado = () => {
+ 
+  let value = document.querySelector("#titulo").value; // Caputar el value del input
+ 
+  if (value.length === 0) {
+    console.log('el usuario no escribio nada,')
+  } else {
+  
+   for (let i = 0; i < value.length ; i++) {
+    const element = value[i];
+    if(element === value[i]) {
+       console.log(value)
+        if (secreto === value) {
+            alert('SECRETO MAGICO');
+            break;
+        }
+        
+    } else {
+        console.log('wrong');
+    }
+    
+   } */
+  
+    // value.forEach((element, index)  => {
+    
+    //   });
+  
+ 
+
+  
+
+};
